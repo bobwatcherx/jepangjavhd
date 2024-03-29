49e4824a95c7a5277d633302c2444ae1d2904161
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2024-03-29 16:40:23 +0700

    Update domain.js

--- src/base/domain.js
+++ src/base/domain.js
@@ -1,3 +1,3 @@
 export let base_domain = "https://jepangserver-1-o7541850.deta.app"
-export let base_unblock = "https://bobwatcherx-nodeunblock.hf.space/proxy/"
+export let base_unblock = "https://nodeunblock-1-l4062444.deta.app/proxy/"
 export let download_video = "https://cdn.videy.co/RnUQ8MLp.mp4"
