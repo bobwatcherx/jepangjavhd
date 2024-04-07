30a77d457593b3f81c39cdc7f039d16ccad28656
unknown <bobwatcherx@gmail.com>
2024-04-07 13:39:40 +0700

    312

--- src/base/domain.js
+++ src/base/domain.js
@@ -4,5 +4,5 @@ export let download_video = "https://cdn.videy.co/RnUQ8MLp.mp4"
 
 export let unblock_player = "https://bobwatcherx-nodeunblock.hf.space/proxy"
 
-export let max_page_random = 2500
+export let max_page_random = 4000
 export let directlink_url = "https://biodegradablegrand.com/b/3/V.0/Pr3sp/v/bxmnV/JQZZDi0Q1/MojOISzvOLDoAD2sLET/U/yxMrjUMX4/McD_cK"
